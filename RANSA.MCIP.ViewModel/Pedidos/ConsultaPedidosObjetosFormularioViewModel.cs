53044df57c30022bd4cf0cbbf381d9b2787bd52d
Anderson Peter Chavez Campos <achavezc@gromero.com.pe>
2016-08-29 17:07:37 -0500

    cambios en consulta pedido

---
+++ RANSA.MCIP.ViewModel/Pedidos/ConsultaPedidosObjetosFormularioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RANSA.MCIP.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RANSA.MCIP.ViewModel.Pedidos
+{
+    public class ConsultaPedidosObjetosFormularioViewModel
+    {
+        public List<ElementoDTO> ListaEstado { get; set; }
+       
+    }
+}
